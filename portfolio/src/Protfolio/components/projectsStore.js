9070924535fd7f07405af054c1c55129cdffdcbc
VIJAY R B <vijaybalajivj@gmail.com>
2021-04-15 12:26:01 +0530

    Add files via upload

---
+++ portfolio/src/Protfolio/components/projectsStore.js
@@ -0,0 +1,81 @@
+import tictactoe from '../projects_images/tictactoe-min.png'
+import secretmessage from '../projects_images/Secret-message-min.png'
+import freshdesk from '../projects_images/freshdesk-min.png'
+import receipe from '../projects_images/receipe-min.png'
+import blog from '../projects_images/blog-min.png'
+import webscrap from '../projects_images/webscrap-min.png'
+import otp from '../projects_images/otp-min.png'
+import protfolio from "../projects_images/protfolio-min.png"
+import sign from "../projects_images/signup-min.png"
+
+var projects_store = [
+    {
+        name: 'Tic Tac Toe Game',
+        image: tictactoe,
+        url: 'https://loving-ptolemy-29cd4f.netlify.app/',
+        github:"https://github.com/vijaybalajirb/Tic-Tac-Toe-game/tree/main/tictactoe",
+        catagory: ['react.js']
+
+    },
+    {
+        name: 'Secret Message',
+        image: secretmessage,
+        url: 'https://mystifying-wright-ea494e.netlify.app/',
+        github:"",
+        catagory: ['fullstack']
+    },
+    {
+        name: 'Freshdesk',
+        image: freshdesk,
+        url: 'https://cranky-borg-ca3a64.netlify.app/',
+        github:"",
+        catagory: ['html','css', 'bootstrap','freshdesk-api']
+    },
+    {
+        name: 'Recipe finder',
+        image: receipe,
+        url: 'https://loving-villani-470eab.netlify.app/?',
+        github:"",
+        catagory: ['api','html','css','bootstrap']
+    },
+    {
+        name: 'Clean Blog frontend',
+        image: blog,
+        url: 'https://jolly-tereshkova-050333.netlify.app/',
+        github:"",
+        catagory: ['html','css','bootstrap']
+    },
+    {
+        name: 'Portfolio',
+        image:protfolio ,
+        url: 'https://vijay-protfolio.netlify.app/',
+        github:"https://github.com/vijaybalajirb/Protfolio",
+        catagory: ['react.js']
+    },
+    {
+        name: 'Web-Scraping',
+        image:webscrap ,
+        url: 'https://friendly-ritchie-055a4e.netlify.app/',
+        github:"https://github.com/vijaybalajirb/Webscrap-frontend",
+        catagory: ['fullstack']
+
+    },
+    {
+        name: 'Otp-login',
+        image:otp ,
+        url: 'https://distracted-ride-2e13cc.netlify.app/',
+        github:"https://github.com/vijaybalajirb/OTP-login-frontend",
+        catagory: ['fullstack']
+    
+    },
+    {
+        name: 'Signup with mongodb and jwt',
+        image:sign ,
+        url: 'https://quirky-kirch-1ce3bc.netlify.app/',
+        github:"https://github.com/vijaybalajirb/signup-with-mongodb-jwt-frontend",
+        catagory: ['fullstack']
+    
+    }
+    
+]
+export default projects_store
